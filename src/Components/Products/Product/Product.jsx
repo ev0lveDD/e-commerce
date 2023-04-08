fe7d197f68a5e5344b9a0cfaad2bec3bf6918994
ev0lveDD <kjagiela93@gmail.com>
2023-04-08 12:57:38 +0200

    testing mobile view

--- src/Components/Products/Product/Product.jsx
+++ src/Components/Products/Product/Product.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function Product({item}) {
     return(
-        <div className="w-11/12 md:w-96 md:h-96 flex flex-row md:flex-col items-center justify-start mx-auto mt-8 border-solid border-2 border-sky-500">
+        <div className="w-11/12 md:w-96 flex flex-row md:flex-col items-center justify-start mx-auto mt-8">
           <div>
-            <img src={item.image} className="w-96 h-48 md:h-64 object-cover border-solid border-2 border-red-500"></img>
+            <img src={item.image} className="w-96 h-48 md:h-80 object-cover"></img>
           </div>
-          <div className="w-full h-16 mt-0 md:mt-6 text-left justify-center ml-4 md:ml-0 flex flex-col">
+          <div className="w-full h-20 mt-0 md:mt-6 text-left justify-center ml-4 md:ml-0 flex flex-col">
             <h2 className="text-xl md:text-lg font-bold">{item.name}</h2>
             <p className="text-gray-500 font-medium mt-1">{item.description}</p>
             <p className="text-lg font-bold mt-2">${item.price}</p>
