58edfcb5083caea349d56537571a420842e25d8d
ev0lveDD <kjagiela93@gmail.com>
2023-04-22 11:13:16 +0200

    Added react-responsive-carousel

--- src/Components/Products/Carousel/Carousel.jsx
+++ src/Components/Products/Carousel/Carousel.jsx
@@ -4,16 +4,16 @@ import { Carousel } from "react-responsive-carousel";
 
 export default () => (
         <Carousel emulateTouch={true} showThumbs={false} showStatus={false} className="m-auto md:hidden">
-            <div>
+            <div className="w-full">
                 <img src={require("../../../Images/jordan-white.jpg")} />
             </div>
-            <div>
+            <div className="w-full">
                 <img src={require("../../../Images/jordan-white2.jpg")} />
             </div>
-            <div>
+            <div className="h-96">
                 <img src={require("../../../Images/jordan-white3.jpg")} />
             </div>
-            <div>
+            <div className="h-96">
                 <img src={require("../../../Images/jordan-white4.jpg")} />
             </div>
         </Carousel>
--- src/Components/Products/Image_Grid/Image_Grid.jsx
+++ src/Components/Products/Image_Grid/Image_Grid.jsx
@@ -22,12 +22,17 @@ function Image_Grid() {
                 </div>
             </div>
             {/* <!-- Right side description --> */}
-            <div className="flex flex-col md:my-10 md:w-2/5 mx-10">
+            <div className="flex flex-col md:my-10 w-full md:w-2/5 mx-4 md:mx-10">
                 {/* <!-- Item name --> */}
                 <div className="flex w-full">
                     <h2 className="text-2xl md:text-lg font-bold text-gray-800">Nike Jordan</h2>
                 </div>
-                
+                                    
+                {/* <!-- Item price --> */}
+                <div className="flex w-full mt-4">
+                    <p className="text-xl font-bold text-gray-700">$350</p>
+                </div>
+
                 {/* <!-- Item Description --> */}
                 <div className="flex flex-col justify-start items-start w-full mt-4">
                     <p className="text-left text-gray-700 font-bold">Description</p>
@@ -35,14 +40,9 @@ function Image_Grid() {
                      Meet the original highflier. With era-echoing '80s construction, bold details and nothing-but-net style, this premium Nike Air Force 1 High '07 brings you everything you know best about the AF1 family. Crisp leather and soft suede keep the look classic while a smoky gray outsole adds the perfect amount of flash. Plus, a padded, high-cut collar with hook-and-loop closure lets you slip into heritage hoops comfort.
                     </p>
                 </div>
-                    
-                {/* <!-- Item price --> */}
-                 <div className="flex w-full mt-4">
-                    <p className="text-xl font-bold text-gray-700">$ 350</p>
-                </div>
 
                 {/* <!-- Item size --> */}
-                 <div className="grid grid-cols-4 md:grid-cols-6 gap-2 md:gap-8 w-full mt-8 justify-evenly justify-items-center">
+                 <div className="grid grid-cols-4 md:grid-cols-6 gap-2 md:gap-4 lg:gap-8 w-full mt-8 justify-evenly justify-items-center">
                     <button class="w-16 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 focus:border-gray-800 focus:border-2 focus:bg-gray-800 focus:text-white rounded">40</button>
                     <button class="w-16 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 focus:border-gray-800 focus:border-2 focus:bg-gray-800 focus:text-white rounded">41</button>
                     <button class="w-16 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 focus:border-gray-800 focus:border-2 focus:bg-gray-800 focus:text-white rounded">42</button>
