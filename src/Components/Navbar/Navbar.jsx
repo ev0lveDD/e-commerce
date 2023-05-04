033f918b5930c15fd4d9f430079bb5bdfcdf99a0
ev0lveDD <kjagiela93@gmail.com>
2023-05-04 13:41:15 +0200

    New menu added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -17,13 +17,22 @@ function Navbar() {
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                         </svg>
                      </button>
+
+                     {open ?
+                    <button data-collapse-toggle="navbar-sticky" type="button" onClick={() => setOpen(!open)} class="z-40 fixed top-10 right-6 inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-sticky" aria-expanded="false">
+                        <span class="sr-only">Close main menu</span>
+                        <svg className="w-6 h-6" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path></svg>
+                    </button>
+                    : 
                     <button data-collapse-toggle="navbar-sticky" type="button" onClick={() => setOpen(!open)} class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-sticky" aria-expanded="false">
                         <span class="sr-only">Open main menu</span>
-                        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
                     </button>
+                    }
+
                 </div>
-                <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${open ? "block" : "hidden"}`} id="navbar-sticky">
-                    <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
+                <div className={`${!open && "animate-slideOut"} z-30 inset-0 overflow-hidden animate-slideIn bg-white items-center justify-between max-h-screen md:h-auto w-full md:flex md:w-auto md:order-1 ${open ? "fixed" : "hidden"}`} id="navbar-sticky">
+                    <ul class="flex flex-col p-4 md:p-0 mt-40 md:mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
                     <li>
                         <a href="#" class="block py-4 md:py-2 pl-3 pr-4 text-2xl md:text-lg text-gray-900 rounded underline underline-offset-8 md:p-0" aria-current="page">Home</a>
                     </li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
     "./node_modules/flowbite/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideIn: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+
+        slideOut: {
+          '0%': { transform: 'translateX()' },
+          '100%': { transform: 'translateX(100%)' },
+        }
+      },
+      animation: {
+        slideIn: 'slideIn 0.5s ease-in-out',
+        slideOut: 'slideOut 0.5s ease-in-out',
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin')
